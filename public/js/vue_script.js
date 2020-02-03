c79f2120970275c883a8cf6c9743ba29167eefb3
Atn0t <antonjager96@gmail.com>
2020-02-03 14:51:02 +0100

    loop and array solutions for menu

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,7 @@
-function menuItem(itemName, itemAllergene, itemInfo, itemStat, itemImg) {
+function menuItem(itemName, lactose, gluten, itemInfo, itemStat, itemImg) {
   this.itemName = itemName;
-  this.itemAllergene = itemAllergene;
+  this.lactose = lactose;
+  this.gluten = gluten;
   this.itemInfo = itemInfo;
   this.itemStat = itemStat;
   this.itemImg = itemImg;
@@ -11,21 +12,18 @@ return menuItem.itemName + ' ' + menuItem.itemStat;
 }
 
 
-let menuItem1 = new menuItem("Moo Kids Meal", "Contains Sodium", "Unusual taste", "1200 kCal", "https://i.ytimg.com/vi/FJM6wd0ehWQ/maxresdefault.jpg");
-let menuItem2 = new menuItem("Torpedo Meal", "Contains Lactose", "Chefs choice", "2001 kCal", "http://3.bp.blogspot.com/_j1v_Cj26Tyk/TPCOdCKGKRI/AAAAAAAAAFs/MoTq035wB60/s1600/Burger1996.jpg");
-let menuItem3 = new menuItem("The Bleeder", "No allergenes", "Found outside kitchen", "980 kCal", "https://media-cdn.tripadvisor.com/media/photo-s/08/54/51/6e/nasty-burger.jpg");
-let menuItem4 = new menuItem("Money Shot Meal", "Contains poison", "For your enemies", "-100 hp", " ");
-let menuItem5 = new menuItem("Heart Stopper", "Contains Rat meat", "New and funky", "555 kCal", " ");
+let menuItem1 = new menuItem("Moo Kids Meal", false, false , "Unusual taste", "1200 kCal", "https://i.ytimg.com/vi/FJM6wd0ehWQ/maxresdefault.jpg");
+let menuItem2 = new menuItem("Torpedo Meal", true, false , "Chefs choice", "2001 kCal", "http://3.bp.blogspot.com/_j1v_Cj26Tyk/TPCOdCKGKRI/AAAAAAAAAFs/MoTq035wB60/s1600/Burger1996.jpg");
+let menuItem3 = new menuItem("The Bleeder", true, true , "Found outside kitchen", "980 kCal", "https://media-cdn.tripadvisor.com/media/photo-s/08/54/51/6e/nasty-burger.jpg");
+let menuItem4 = new menuItem("Money Shot Meal", false, true, "For your enemies", "-100 hp", " ");
+let menuItem5 = new menuItem("Heart Stopper", false, false,  "New and funky", "555 kCal", " ");
+
 
 
 const vm = new Vue({
 el: '#myID',
 data: {
-  titel: 'Välj en burgare',
-  mookids: nameAndKcal(menuItem1),
-  torpedo: nameAndKcal(menuItem2),
-  bleeder: nameAndKcal(menuItem3),
-  money: nameAndKcal(menuItem4),
-  heartstopper: nameAndKcal(menuItem5)
+titel: 'Välj en burgare',
+burgers: [menuItem1, menuItem2, menuItem3, menuItem4, menuItem5]
 }
 })
